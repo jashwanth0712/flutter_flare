1ba9e20469c5d2e45ff750fcd109a1b1814bdfee
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2022-01-05 22:14:13 +0530

    a

---
+++ lib/flare_demo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flare/smart_flare_animation.dart';
+class FlareDemo extends StatefulWidget {
+  @override
+  _FlareDemoState createState() => _FlareDemoState();
+}
+
+class _FlareDemoState extends State<FlareDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 102, 18, 222),
+        body: Align(
+            alignment: Alignment.bottomCenter,
+            child: SmartFlareAnimation()));
+  }
+}
